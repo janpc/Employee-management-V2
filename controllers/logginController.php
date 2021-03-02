3003f3d7d752352cd3b444518ff39016b398758f
Jan Puig <janpuigcapella@gmail.com>
2021-03-02 09:30:59 +0100

    delete comment

--- controllers/logginController.php
+++ controllers/logginController.php
@@ -30,20 +30,3 @@ class LogginController extends Controller
 
 
 }
-
-/* require('loginManager.php');
-
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-if (isset($email)) {
-    $hasLoggedIn = logIn($email, $password);
-    if ($hasLoggedIn) {
-        header('Location: ../dashboard.php');
-    } else {
-        header('Location: ../../index.php?error=true');
-    }
-} else {
-    $hasLoggedOut = logOut();
-    header('Location: ../../index.php');
-} */
