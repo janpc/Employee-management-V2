aa68f1bc430c848fa9478fe12cfed2b6dcd266d4
Jan Puig <janpuigcapella@gmail.com>
2021-03-02 17:32:36 +0100

    first aproach

--- views/characters/detail.php
+++ views/characters/detail.php
@@ -1,3 +1,50 @@
-<?php
-// character details
-print_r($this->data);
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $this->data['name'] ?></title>
+    <link rel="stylesheet" href="/employee-management-v2/assets/css/characterDetails.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <section >
+        <form action="" method="post" class='infoContainer'>
+            <div class='info_column'>
+                <img src="https://rickandmortyapi.com/api/character/avatar/<?php echo $this->data['id'] ?>.jpeg" alt="">
+                <label for="origin_loc">Origin:</label>
+                <div>
+                    <a type="text"  href="<?php echo "http://" . $_SERVER['SERVER_NAME'] . '/employee-management-v2/location/details/' . $this->data['origin_loc']['id'] ?>">></a>
+                    <select name="origin_loc" id="origin_loc">
+                        <option value="earth">earth</option>
+                    </select>
+                </div>
+                <label for="last_loc">Last Seen:</label>
+                <div>
+                    <a type="text" name="last_loc" id="last_loc" href="<?php echo "http://" . $_SERVER['SERVER_NAME'] . '/employee-management-v2/location/details/' . $this->data['last_loc']['id'] ?>">></a>
+                    <select name="origin_loc" id="origin_loc">
+                        <option value="earth">earth</option>
+                    </select>
+                </div>
+            </div>
+            <div class='info_column'>
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name" value='<?php echo $this->data['name'] ?>'>
+                <label for="status">Status:</label>
+                <input type="text" name="status" id="status" value='<?php echo $this->data['status'] ?>'>
+                <label for="species">Specie:</label>
+                <input type="text" name="species" id="species" value='<?php echo $this->data['species'] ?>'>
+                <label for="gender">Gender:</label>
+                <input type="text" name="gender" id="gender" value='<?php echo $this->data['gender'] ?>'>
+                <input type="submit" value="Save">
+            </div>
+
+
+        </form>
+    </section>
+</body>
+
+</html>
