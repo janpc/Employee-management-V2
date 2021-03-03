8991bdb0021c32c8a74b1a0dcec5851e8856775d
janpc <64541042+janpc@users.noreply.github.com>
2021-03-03 16:59:35 +0100

    Character view (#14)

--- models/entity/CharacterExt.php
+++ models/entity/CharacterExt.php
@@ -3,15 +3,17 @@
 class CharacterExt {
     public Int $id;
     public String $name;
+    public String $status;
     public String $species;
     public String $gender;
     public Location $originLoc;
     public Location $lastLoc; 
 
-    public function __construct($id, $name, $species, $gender, $originLoc, $lastLoc)
+    public function __construct($id, $name, $status, $species, $gender, $originLoc, $lastLoc)
     {
         $this->id = $id;
         $this->name = $name;
+        $this->status = $status;
         $this->species = $species;
         $this->gender = $gender;
         $this->originLoc = $originLoc;
---
+++ util/htmlLinks.php
@@ -0,0 +1,7 @@
+<?php
+
+echo '<link href="https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="/employee-management-v2/assets/css/header.css">
+
+<script defer src="/employee-management-v2/assets/js/characterDetails.js"></script>';
