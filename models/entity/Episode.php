72eaf97d13b202713ee08bb261bf6dbff758ce64
janpc <64541042+janpc@users.noreply.github.com>
2021-03-04 14:58:40 +0100

    Character view finished (#17)
    * first aproach
    
    * character view dessign
    
    * prueba
    
    * p2
    
    * characters views

--- models/entity/Episode.php
+++ models/entity/Episode.php
@@ -6,12 +6,14 @@ class Episode
     public ?string $airDate;
     public ?int $seasonNo;
     public ?int $episodeNo;
+    public ?string $name;
 
-    public function __construct($id = null, $airDate = null, $seasonNo = null, $episodeNo = null)
+    public function __construct($id = null, $name = null, $airDate = null, $seasonNo = null, $episodeNo = null)
     {
         $this->id = $id;
         $this->airDate = $airDate;
         $this->seasonNo = $seasonNo;
         $this->episodeNo = $episodeNo;
+        $this->name = $name;
     }
 }
